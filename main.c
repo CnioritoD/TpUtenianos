2217e864c575a507b787515fed2d7c25fd29d8ec
CnioritoD <CnioritoD@gmail.com>
2026-06-01 02:36:00 -0300

    bug del parser arreglado (ahora compila bien en terminal)

--- main.c
+++ main.c
@@ -3,16 +3,19 @@
 
 int main() {
     char entrada[256];
-    
-    printf("--- Compilador IoT: Analisis Lexico y Sintactico ---\n");
+
+    printf("--- Compilador IoT: Analisis Lexico ---\n");
     printf("Escribi tu codigo de prueba:\n> ");
-    
+
     if (fgets(entrada, sizeof(entrada), stdin) != NULL) {
         const char *puntero_cadena = entrada;
-        
-        // En vez de hacer el bucle pidiendo tokens, le pasamos el control al Parser
-        iniciar_parser(&puntero_cadena);
+        Token token;
+
+        do {
+            token = obtener_siguiente_token(&puntero_cadena);
+            printf("%s -> %s\n", nombre_token(token.tipo), token.valor);
+        } while (token.tipo != TOKEN_EOF && token.tipo != TOKEN_ERROR);
     }
-    
+
     return 0;
 }
